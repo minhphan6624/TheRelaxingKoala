ac848850d4e4840c4604aa7c5c96702db297a795
Brandon0230 <103169140+Brandon0230@users.noreply.github.com>
2024-05-07 17:59:15 +1000

    create basic menuitem class :)

---
+++ MenuItem.js
@@ -0,0 +1,8 @@
+class MenuItem {
+  constructors(itemName, description='', price, imagePath='') {
+    this.itemName = itemName;
+    this.description = description;
+    this.price = price;
+    this.imagePath = imagePath;
+  }
+}
