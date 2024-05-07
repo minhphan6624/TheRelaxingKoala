d7834b6f3f905160fdc0e9e81d479cf78242930c
Brandon0230 <103169140+Brandon0230@users.noreply.github.com>
2024-05-07 17:46:29 +1000

    create basic user class constructors

--- User.js
+++ User.js
@@ -1,1 +1,7 @@
-
+class User  {
+  constructor(firstName, lastName, mobileNumber) {
+      this.firsName = firstName; 
+      this.lastName = lastName;
+      this.mobileNumber = mobileNumber;
+  }
+};
