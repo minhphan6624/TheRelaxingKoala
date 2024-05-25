814a92bed8ce07f2727b9e3169b0dbd338c21416
Brandon0230 <103604188@student.swin.edu.au>
2024-05-25 13:28:57 +1000

    create Menuitem

---
+++ Models/Menu/MenuItem.js
@@ -0,0 +1,31 @@
+class MenuItem {
+  constructor(name, price, isFood, imagePath, description) {
+    this.name = name;
+    this.price = price;
+    this.isFood = isFood;
+    this.imagePath = imagePath;
+    this.description = description;
+  }
+    get name() {
+      return this.name;
+    }
+    get price() {
+      return this.price;
+    }
+    get isFood() {
+      return this.isFood;
+    }
+    get description() {
+      return this.description;
+    }
+
+    updateItemName(itemName) {
+      this.name = itemName;
+    }
+    updateItemPrice(itemPrice) {
+      this.price = itemPrice;
+    }
+    updateImagePath(imagePath) {
+      this.imagePath = imagePath;
+    }
+}
