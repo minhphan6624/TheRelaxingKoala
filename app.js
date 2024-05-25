527570094d5078b61a17edeed022e4044307adf4
Brandon0230 <103604188@student.swin.edu.au>
2024-05-25 13:20:03 +1000

    new

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 
 const express = require('express')
-const sequelize = require('./Config/database');
+//const sequelize = require('./Config/database');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 //Set up view engine
-app.set('view engine', 'ejs');
-
+//app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'Public')));
+app.use(express.static(path.join(__dirname, 'Views')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Views, index.html'));
+});
 
 //Start the server
 app.listen(port, () => {
