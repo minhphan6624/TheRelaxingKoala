586f6ab59fd8ba499458bc024aa19ad8a3efc4e7
minhphan6624 <quangminh6624@gmail.com>
2024-05-28 09:19:31 +1000

    Fixed home page problem

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ const app = express();
 const port = 3000;
 
 //Set up view engine
-//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
+
 app.use(express.static(path.join(__dirname, 'Public')));
 app.use(express.static(path.join(__dirname, 'Views')));
+
+//Home page route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Views, index.html'));
+    res.render('index');
 });
+
+//Menu route
+app.get('/menu', (req, res) => {
+    res.render('menu')
+})
 
 //Start the server
 app.listen(port, () => {
