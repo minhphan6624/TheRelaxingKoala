a415b41ca1dd8794d576cdf16c1db81cb8b28528
Brandon0230 <103169140+Brandon0230@users.noreply.github.com>
2024-05-30 20:17:49 +1000

    Update orderController.js

--- Controllers/orderController.js
+++ Controllers/orderController.js
@@ -1,80 +1,31 @@
-const Order = requrie('../Models/Order');
 
-// ---------------------CREATE operations---------------------
-exports.createOrder = (req, res) => {
-    //Create a new Order object using the request body
-    const order = new Order(req.body);
+import MenuItem from '../Models/Menu/MenuItem.mjs';
+import Menu from '../Models/Menu/Menu.mjs';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const sqlite3 = require('sqlite3');
+const { Database } = sqlite3.verbose();
 
-    //Store the order information to the database instace
-    order.save((err, newOrderID) => {
-        if (err) {
-            return res.status(500).json({ error: err });
-        }
-        
-        return res.status(201).json(newOrderID);
-    })
+const db = new Database('../database.sqlite');
+function main() {
+    let menuItemArray = [];
+    menuItemArray[0] = new MenuItem('Burger', 10, true, 'burger.jpg', 'A delicious burger');
+    menuItemArray[1] = new MenuItem('Fries', 5, true, 'fries.jpg', 'A delicious fries');
+    let menu = new Menu(menuItemArray);
+    console.log(menu.getMenuItemByIndex(0));
+    writeMenuToDB(menu);
 }
-
-// ---------------------READ Operations---------------------
-exports.getAllOrders = (req, res) => {
-
-    //Fetch all recorded orders from the DB
-    Order.getAll((err, orders) => {
-        if (err) {
-            return res.status(500).json({ error: err });
+function writeMenuToDB(menu) {
+    // Write the menu to the database
+    db.serialize(() => {
+        db.run('CREATE TABLE IF NOT EXISTS menuitems ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT, price INTEGER, category VARCHAR(255), available TINYINT(1), createdAt DATETIME, updatedAt DATETIME)');
+        let stmt = db.prepare('INSERT INTO menuitems VALUES (?,?,?,?,?,?,?,?)');
+        console.log(menu.length());
+        console.log(menu.menuItems[0].id);
+        for (let i = 0; i < menu.length(); i++) {
+            stmt.run(menu.menuItems[i].id, menu.menuItems[i].name, menu.menuItems[i].description, menu.menuItems[i].price, menu.menuItems[i].isFood, menu.menuItems[i].available, new Date().toISOString(), new Date().toISOString());
         }
-
-        return res.status(200).json(orders);
-        
-    })
-}
-
-exports.getOrderByID = (req, res) => {
-    Order.findById(req.params.id, (err, order) => {
-        if (err) {
-            return res.status(500).json({ error: err });
-        }
-        else if (!order) {
-            return res.status(404).json({ error: 'Order not found!' });
-        }
-        
-        return res.status(200).json(order );
-        
-    })
-}
-
-//---------------------UPDATE Operations---------------------
-exports.updateOrder = (req, res) => {
-
-    Order.findById(req.params.id, (err, order) => {
-        if (err) {
-            return res.status(500).json({ error: err.message }); //Server error
-        }
-        // If there is no matching order to be found
-        else if (!order) {
-            return res.status(404).json({ error: "Order not found" }); //Return a 404 error
-        }
-        
-        //Update the order using the request body
-        order.updateOrderDetails(req.body, (err) => {
-            if (err) {
-                return res.status(500).json({ error: err.message }); //Server error
-            } 
-            
-            return res.status(200).json({message: 'Order updated'});
-            
-        });
-        
+        stmt.finalize();
     });
 }
-
-//---------------------DELETE Operations---------------------
-exports.deleteOrder = (req, res) => {
-    Order.delete(req.params.id, (err, result) => {
-        if (err){
-            return res.status(500).json({err: err.message})
-        }
-
-        return res.status(200).json(result);
-    })
-}
+main();
