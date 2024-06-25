0fa2b1d643eb6950aaba64706ec672ea5e5238d2
Minh Phan <113814582+minhphan6624@users.noreply.github.com>
2024-06-25 18:30:48 +1000

    The Revamp Koala

--- config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-// Initialize Sequelize with SQLite
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite' // Path to SQLite database file
-});
-
-module.exports = sequelize;
