3b1c801f36ad4bff213a3ba5f6c61f3154757074
Minh Phan <113814582+minhphan6624@users.noreply.github.com>
2024-06-25 19:15:58 +1000

    created endpoints for order management

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const port = 3000;
 app.use(express.json()); //Parse JSON bodies
 
 //Import routes
-reservationRoutes = require('./routes/reservationsRoutes');
+const reservationRoutes = require('./routes/reservationsRoutes');
+const orderRoutes = require('./routes/ordersRoutes');
 
-app.use('api/reservations', reservationRoutes) // Reservation routes
+app.use('/api/reservations', reservationRoutes) // Reservation routes
+app.use('/api/orders', orderRoutes) // Order routes
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
