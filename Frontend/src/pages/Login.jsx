f18aeac5f6041783ad5687b51792a476f8420aa2
minhphan6624 <quangminh6624@gmail.com>
2024-07-31 10:51:18 +1000

    css changes

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -13,33 +13,37 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        try 
-        {
-            const response = await axios.post('http://localhost:3000/api/auth/login', {username, password});
-            
+        try {
+            const response = await axios.post('http://localhost:3000/api/auth/login', { username, password });
+
             const token = response.data.token; // Extract token from response
 
             localStorage.setItem('token', token); //Store token in 
             window.location.href = '/'; // Redirect to home page after login
         }
-        catch (error)
-        {
+        catch (error) {
             console.error('Error logging in:', error);
             alert('Failed to login');
         }
     }
 
     return (
-        <div className="login-page">
+        <div className="login-page container d-flex flex-column justify-content-center align-items-center">
             <form className="login-form" onSubmit={handleSubmit}>
                 <h2>Login</h2>
-                <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}/>
-                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                <button type="submit" className="login-btn">Login</button>
+                <div className="form-group">
+                    <label>Username:</label>
+                    <input className="form-control" id="username" type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Password:</label>
+                    <input className="form-control" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <button type="submit" className="btn btn-primary">Login</button>
 
                 <div className="sign-in link">
                     <p>Don't have an account? <Link to="/register">Sign up </Link></p>
-            </div>
+                </div>
             </form>
         </div>
     )
